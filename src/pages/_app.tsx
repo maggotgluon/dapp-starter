565fb6117510c5d09d12ba2c48158b2f8193292b
Ruttaphong.w <maggotgluon@gmail.com>
2021-12-06 14:29:23 +0700

    clear

--- src/pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-import type { AppProps } from "next/app";
-
-import "../index.css";
-
-const MyApp = function ({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-};
-
-export default MyApp;
